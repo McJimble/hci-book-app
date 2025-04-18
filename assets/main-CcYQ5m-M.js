9bb8c33e6a951b01be5c5859ca1816be7cd0dcb5
James Robertson <jimmyrobertsonv@gmail.com>
2025-04-18 03:48:11 -0500

    Updates

--- assets/main-CcYQ5m-M.js
+++
@@ -1,2 +0,0 @@
-import{$ as t}from"./jquery-DQo7jxWw.js";const n=document.querySelector("#username-input");window.addEventListener("DOMContentLoaded",async()=>{t(function(){const e=document.getElementById("login-button");e==null||e.addEventListener("click",()=>{localStorage.setItem("username",n.value),window.location.href="/pages/browse.html"}),localStorage.setItem("username","User123")})});
-//# sourceMappingURL=main-CcYQ5m-M.js.map
